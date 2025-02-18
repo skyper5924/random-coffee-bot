2bd3bb3798843570f8d35825d56d5797ed7f7e7c
skyper5924 <ivanmokrusin62196@gmail.com>
2025-02-18 16:48:57 +0300

    v1.1 Добавлен выбор тем

--- keyboards/admin_menu.py
+++ keyboards/admin_menu.py
@@ -4,6 +4,7 @@ admin_menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Количество участников")],
         [KeyboardButton(text="Отправить сообщение всем")],
+        [KeyboardButton(text="Управление темами")],  # Новая кнопка
         [KeyboardButton(text="Вернуться в главное меню")]
     ],
     resize_keyboard=True
