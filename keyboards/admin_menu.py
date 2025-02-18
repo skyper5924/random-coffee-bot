3119c5fb3b21807b4da3f21a19618b1c475681b1
skyper5924 <ivanmokrusin62196@gmail.com>
2025-02-18 16:51:13 +0300

    Revert "v1.1"
    This reverts commit 2bd3bb3798843570f8d35825d56d5797ed7f7e7c.

--- keyboards/admin_menu.py
+++ keyboards/admin_menu.py
@@ -4,7 +4,6 @@ admin_menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Количество участников")],
         [KeyboardButton(text="Отправить сообщение всем")],
-        [KeyboardButton(text="Управление темами")],  # Новая кнопка
         [KeyboardButton(text="Вернуться в главное меню")]
     ],
     resize_keyboard=True
